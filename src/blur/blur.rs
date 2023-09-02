e869c87a5f0a1bed224bae3cac55c1db37f67521
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-09-03 02:59:26 +0600

    blur

---
+++ src/blur/blur.rs
@@ -0,0 +1,15 @@
+use std::path::PathBuf;
+
+use image::imageops::blur;
+
+pub fn blur_image() {
+    let path = PathBuf::from("assets/food.jpg");
+    let seve_path = PathBuf::from("src/blur");
+
+    let img = image::open(path).unwrap();
+    let sigma = 20.0;
+    let blured_image = blur(&img, sigma);
+    blured_image
+        .save(seve_path.join("blured_image.png"))
+        .unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-use fractal::fractal::fractal_image;
-use gradient::gradient::gradient_image;
-use scaledown::scaledown::scaledown_image;
-use scaleup::scaleup::scaleup_image;
-use tile::tile::tile_image;
+use blur::blur::blur_image;
 
+pub mod blur;
 pub mod fractal;
 pub mod gradient;
 pub mod scaledown;
@@ -11,9 +8,5 @@ pub mod scaleup;
 pub mod tile;
 
 fn main() {
-    fractal_image();
-    gradient_image();
-    scaledown_image();
-    scaleup_image();
-    tile_image();
+    blur_image();
 }
